c56b12db490fb03fa66387bf2cbd153c7805fd4d
K Ramkrishna Achary <kramkrishnaachary@Rams-MacBook-Pro.local>
2025-03-18 22:37:34 +0530

    updated the test file

--- test.py
+++ test.py
@@ -1,17 +1,32 @@
 from pyspark.sql import SparkSession
+import getpass
 import time
+username = getpass.getuser()
 
-# Create Spark session
-spark = SparkSession.builder.appName("Spark Test").getOrCreate()
+spark = SparkSession. \
+builder. \
+config('spark.ui.port', '0'). \
+config('spark.shuffle.useOldFetchProtocol', 'true'). \
+config("spark.sql.warehouse.dir", f"/user/{username}/warehouse"). \
+enableHiveSupport(). \
+master('local'). \
+getOrCreate()
 
-print("Spark session created", spark)
+print("Initiated Spark session with Hive support")
+# print(f"Spark version: {spark.version}") 
 
-# Create DataFrame
-data = [("John", "Doe", 28), ("Jane", "Doe", 25), ("John", "Smith", 30)]
-columns = ["first_name", "last_name", "age"]
-df = spark.createDataFrame(data, columns)
-df.show()
+path = "/Users/kramkrishnaachary/Learning/Trendytech/lending_club_project/data/accepted_2007_to_2018Q4.csv"
+raw_df = spark.read \
+.format("csv") \
+.option("InferSchema","true") \
+.option("header","true") \
+.load(path)
 
-# Keep the session active to access the UI
-print("Access Spark UI at http://localhost:4040")
-time.sleep(300)  # Keep the session active for 5 minutes
+print("Loaded data from CSV")
+
+raw_df.printSchema()
+raw_df.show(5, truncate=False)
+
+# # Keep the session active to access the UI
+# print("Access Spark UI at http://localhost:4040")
+# time.sleep(300)  # Keep the session active for 5 minutes
